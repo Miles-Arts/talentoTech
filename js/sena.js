d2cefb32da80f3c1f23177183f56b03e78ed8bbd
Milton Figueredo Miles <gratno@gmail.com>
2024-06-30 14:18:46 -0500

    pruebas SENA

---
+++ js/sena.js
@@ -0,0 +1,21 @@
+/* Evidencia de desempeño: GA3-220501093-AA3-EV02 resolución a problemas algorítmicos aplicando
+estructuras de almacenamiento  */
+
+
+
+/* 1. Desarrollar un programa que permita calcular el área o perímetro 
+de algunas figuras planas según lasiguiente tabla:  */
+
+
+
+// Figuras:  Triangulo, Rectangulos, Cuadrado, Circulo
+
+
+
+var a = 0;
+var b = 0;
+var c = 0;
+
+var perimetroCuadrado = a + b + c;
+
+console.log("El perímetro del triángulo es de: " + perimetroCuadrado );
