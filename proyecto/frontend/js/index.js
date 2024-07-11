f73b3fbe245a942768d0d52b09a68349445b11bd
Milton Figueredo Miles <gratno@gmail.com>
2024-07-10 21:43:42 -0500

    JS dinámico for HTML

--- proyecto/frontend/js/index.js
+++ proyecto/frontend/js/index.js
@@ -14,14 +14,25 @@ document.addEventListener("DOMContentLoaded", function () {
         const cardSection = document.getElementById("i-card-section")
 
         const bosques = [
-             {nombre: "Pino", especia: "arbolis", img: "pino.jpg", visto: "Tuta", fecha: new Date(2024, 3, 14 )  }, 
-             {nombre: "Árbol Agua", especia: "arbolos", img:"agua.jpg", visto: "Paipa", fecha: new Date(2024, 5, 22 )}
+             {nombre: "Pino", especie: "arbolis", img: "pino.jpg", visto: "Tuta", fecha: new Date(2024, 3, 14 )  }, 
+             {nombre: "Árbol Agua", especie: "arbolos", img:"agua.jpg", visto: "Paipa", fecha: new Date(2024, 5, 22 )}
             ] 
 
            bosques.forEach( bosque => { 
             const bosqueElemento = document.createElement("div")
             div.className = "col-md-4 "
-            div.innerHTML =  "<p>Hola</p>"
+            div.innerHTML =  `  
+                        <div class="card" style="width: 18rem;">
+                            <img src="./img/${bosque.img}" class="card-img-top" alt="Imagen de Bosque de ${bosque.nombre}">
+                            <div class="card-body">
+                                <h5 class="card-title">${bosque.nombre }</h5>
+                                <p class="card-text">${bosque.especie}</p>
+                                <p class="card-text">${bosque.visto}</p>
+                                <p class="card-text">${bosque.toLocaleDateString()}</p>
+                                <a href="#" class="btn btn-primary">Ver más</a>
+                            </div>
+                        </div>
+            ` 
 
             cardSection.appendChild(div);
 
