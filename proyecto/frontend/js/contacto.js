37d9443cc6cee22ff94d0083019d3b89b5714b41
Milton Figueredo Miles <gratno@gmail.com>
2024-07-10 23:01:51 -0500

    add Js a servicios

--- proyecto/frontend/js/contacto.js
+++ proyecto/frontend/js/contacto.js
@@ -1,3 +1,30 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
 
-});
+    const manejoFormulario= ()=>{
+
+        const form= document.getElementById("i-contacto-form")
+
+        form.addEventListener("submit", function(event){
+            event.preventDefault()
+
+            const nombre= document.getElementById("nombre").value
+            const email= document.getElementById("email").value
+            const mensaje= document.getElementById("mensaje").value
+            const terminos= document.getElementById("terminos").checked
+
+            if(!nombre || !email || !mensaje || !terminos){
+                alert("Por favor, completa los campos vacios.")
+                return
+            }
+
+            console.log(`Formulario enviado con nombre ${nombre}, email ${email}, mensaje ${mensaje}, terminos ${terminos}`)
+            alert("Formulario enviado.")
+
+            form.reset()
+
+        })
+    }
+
+    manejoFormulario()
+
+})
