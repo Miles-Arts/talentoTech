e7c6e4bbf086b98c9cef5541dd12094aa87d7fb1
Milton Figueredo Miles <gratno@gmail.com>
2024-07-10 21:00:01 -0500

    add js array images

--- proyecto/frontend/js/index.js
+++ proyecto/frontend/js/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
 
+
+    function nombre() {
+
+    }
+
+    const nombre2 = function() {
+
+    }
+
+    const mostrarBosque =  () => {
+
+        const cardSection = document.getElementById("i-card-section")
+
+        const bosques = [
+             {nombre: "Pino", especia: "arbolis", img: "pino.jpg", visto: "Tuta", fecha: "2024"}, 
+             {nombre: "Árbol Agua", especia: "arbolos", img:"agua.jpg", visto: "Paipa", fecha: "2024"}
+            ] 
+
+    }
+
+    mostrarBosque()
+
 });
