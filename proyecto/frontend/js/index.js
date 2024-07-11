c05a94ff67779dba4b4eb19d47bc638d2074adf2
Milton Figueredo Miles <gratno@gmail.com>
2024-07-10 21:22:49 -0500

    add js index innetHTML

--- proyecto/frontend/js/index.js
+++ proyecto/frontend/js/index.js
@@ -14,9 +14,18 @@ document.addEventListener("DOMContentLoaded", function () {
         const cardSection = document.getElementById("i-card-section")
 
         const bosques = [
-             {nombre: "Pino", especia: "arbolis", img: "pino.jpg", visto: "Tuta", fecha: "2024"}, 
-             {nombre: "Árbol Agua", especia: "arbolos", img:"agua.jpg", visto: "Paipa", fecha: "2024"}
+             {nombre: "Pino", especia: "arbolis", img: "pino.jpg", visto: "Tuta", fecha: new Date(2024, 3, 14 )  }, 
+             {nombre: "Árbol Agua", especia: "arbolos", img:"agua.jpg", visto: "Paipa", fecha: new Date(2024, 5, 22 )}
             ] 
+
+           bosques.forEach( bosque => { 
+            const bosqueElemento = document.createElement("div")
+            div.className = "col-md-4 "
+            div.innerHTML =  "<p>Hola</p>"
+
+            cardSection.appendChild(div);
+
+           } ) 
 
     }
 
