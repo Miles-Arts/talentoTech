c8b52430dbdd467ba0c0ca3599ab3c6ecd679706
Milton Figueredo Miles <gratno@gmail.com>
2024-07-13 14:00:16 -0500

    Python example

--- webscraping/web.py
+++ webscraping/web.py
@@ -8,29 +8,29 @@ hoja_activa = workbook.active
 
 
 
-website = ''
+# website = 'https://co.mileroticos.com/escorts/boyaca/tunja/'
 
-resultado = requests.get(website)
+url = requests.get('https://co')
 
-soup = BeautifulSoup(resultado.content, 'html.parser' )
+soup = BeautifulSoup(url.content, 'html.parser' )
 
 
 nombre = []
 
-elementos_a = soup.find_all('h1', class_='title_viewad')
+elementos_a = soup.find_all('div', class_='info-container country-co')
 
 for elemento_h2 in elementos_a:
-    elemento_a = elemento_h2.find('a')
+    elemento_a = elemento_h2.find('p')
     if elemento_a:
         nombre.append(elemento_a.text.strip())
 
-todo_junto = [(nombre[i]) for i in range(len(nombre))]
-for producto in todo_junto:
-    print(producto)
+# todo_junto = [(nombre[i]) for i in range(len(nombre))]
+# for producto in todo_junto:
+#     print(producto)
 
-for i in range(len(nombre)):
-    hoja_activa.cell(row=i+1, column=1, value=nombre[i])
+# for i in range(len(nombre)):
+#     hoja_activa.cell(row=i+1, column=1, value=nombre[i])
 
-    workbook.save("nombre.xlsx")
+#     workbook.save("nombre.xlsx")
 
-    print("Holaas")
+print("Holaas")
