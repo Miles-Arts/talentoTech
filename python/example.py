258b6ed2a5db11fdec94fa1bc6b32ac8e7569ad6
Milton Figueredo Miles <gratno@gmail.com>
2024-07-15 18:33:21 -0500

    Example

---
+++ python/example.py
@@ -0,0 +1,45 @@
+# # xc = 3
+# # print(f"Hola {xc}")
+
+# nombre = input ("Ingrese su nombre: ")
+# print(f"Gran gusto tener acá, {nombre}, estás en BackMaria")
+
+# number1 = input("Ingrese el primer número: ")
+# number2 = input("Ingrese el segundo número: ")
+
+# number1 = int(number1)
+# number2 = int(number2)
+
+# sumaq = number1 + number2
+
+# number2 = input(f"La suma de los números {number1} y {number2} es: {sumaq}")
+
+# numero = 1
+# suma = 2 + 4
+# c = suma + numero * 3
+# print("Valor de C es:", c)
+
+# x,y,z = 1, 2, 3
+# print("x =", x, "| y =", y,"| z =", z)
+
+# numero = 5
+# numero = numero + 1
+
+# print("Valor número:", numero)
+
+# numero += 1
+# print("Valor numero:", numero)
+
+numero1 = 2
+numero2 = 5
+print("Suma:", numero1 + numero2 )
+print("Resta:", numero1 - numero2 )
+print("Multiplicación:", numero1 * numero2 )
+print("División:", numero1 / numero2 )
+print("Moódulo:", numero1 % numero2 )
+print("División Entera:", numero1 // numero2 )
+print("Potencia:", numero1 ** numero2 )
+
+
+
+
