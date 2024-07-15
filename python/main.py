b53679218ba12113c77d32b79a04c66e82cae138
Milton Figueredo Miles <gratno@gmail.com>
2024-07-15 07:00:28 -0500

    Concatenacion variables Python

--- python/main.py
+++ python/main.py
@@ -9,10 +9,16 @@
 #Python lee de Derecha a Izquierda
 # #El String se ASIGNA en memeria a X
 x = "Python es" #Inicializacion de la Variable la priemra vez que la usamos. 
-y = " fácil ❤ love"
+y = " fácil ❤  love"
 z =  x + y
 
-print("Cocatenación: " + z)
+palabra1 = "Hola"
+palabraDos = " Talento"
+palabraTres = " Tech"
+union = palabra1 + palabraDos + palabraTres
+
+print("✅ Concatenación: " + z + " 👈")
+print("Buen día: " + union + " 👈")
 
 
 
