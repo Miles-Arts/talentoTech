eae5d1737103b5b9f346c06551531c972404a702
Milton Figueredo Miles <gratno@gmail.com>
2024-07-15 06:41:52 -0500

    Python first steps

--- python/main.py
+++ python/main.py
@@ -1,4 +1,18 @@
 # D:\SENA\USA\BOOTCAMP\python\
 
-print("hola" , 1 + 9999999)
+#print("hola" , 1 + 9999999)
+
+#VAriables
+
+x = "Python es"
+y = " fácil ❤ love"
+z =  x + y
+
+print("Cocatenación: " + z)
+
+
+
+
+
+
 
