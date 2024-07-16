605587f24c9eed5d0f759072e8c4f8d670bbcfe5
Milton Figueredo Miles <gratno@gmail.com>
2024-07-16 14:11:17 -0500

    If anidado

--- python/example.py
+++ python/example.py
@@ -133,26 +133,52 @@ import random
 
 
 #IF elif ELSE----------------------------------------------------
+#  IF ENCADENADO
+#  IF  sentencia con condicion
+#  ELIF  es condicion y sentenciAS
+#  ELSE  sentencias 
+#
 
-edad = input("Ingrese su edad: " )
+# edad = input("Ingrese su edad: " )
+# edad = int(edad)
+
+# if edad < 18:
+#     print(f"Es menor de edad tiene: {edad} años.")
+
+# elif edad >= 18 and edad < 60:
+#     print(f"Es adulto tiene: {edad} años.")
+
+# else:
+#     print(f"Es adulto mayor tiene: {edad} años.")
+
+
+#If ANIDADO--------------------------------------
+#if condicion:
+        # if condicion :
+        # SENTENCIA
+        # else:
+        # SENTENCIA
+        # else:
+        # if condicion :
+        # SENTENCIA
+        # else:
+        # SENTENCIA
+
+edad = input("Ingrese su edad: ")
+
 edad = int(edad)
 
 if edad < 18:
-    print(f"Es menor de edad tiene: {edad} años.")
-
-elif edad >= 18 and edad < 60:
-    print(f"Es adulto tiene: {edad} años.")
+    if edad > 12 and edad < 18:
+        print(f"Es adolecente con {edad} años.")
+    else:
+        print("Está en la niñez") 
 
 else:
-    print(f"Es adulto mayor tiene: {edad} años.")
-
-
-
-
-
-
-
-
+    if edad >= 18 and edad < 60:
+        print(f"Es adulto tiene {edad} años.")
+    else:
+        print(f"Es adulto mayor con {edad} años.")    
 
 
 
