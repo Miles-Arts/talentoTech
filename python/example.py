216ca9cb0799aced71397d800377c5b0340d953e
Milton Figueredo Miles <gratno@gmail.com>
2024-07-16 12:31:04 -0500

    New Python day

--- python/example.py
+++ python/example.py
@@ -1,6 +1,8 @@
 # # xc = 3
 # # print(f"Hola {xc}")
 
+#input---------------------------------------------------------------
+
 # nombre = input ("Ingrese su nombre: ")
 # print(f"Gran gusto tener acá, {nombre}, estás en BackMaria")
 
@@ -14,6 +16,7 @@
 
 # number2 = input(f"La suma de los números {number1} y {number2} es: {sumaq}")
 
+#Operadores asignación------------------------------------------------
 # numero = 1
 # suma = 2 + 4
 # c = suma + numero * 3
@@ -30,15 +33,62 @@
 # numero += 1
 # print("Valor numero:", numero)
 
-numero1 = 2
-numero2 = 5
-print("Suma:", numero1 + numero2 )
-print("Resta:", numero1 - numero2 )
-print("Multiplicación:", numero1 * numero2 )
-print("División:", numero1 / numero2 )
-print("Moódulo:", numero1 % numero2 )
-print("División Entera:", numero1 // numero2 )
-print("Potencia:", numero1 ** numero2 )
+
+#Operadores aritméticos------------------------------------------------
+# numero1 = 2
+# numero2 = 5
+# print("Suma:", numero1 + numero2 )
+# print("Resta:", numero1 - numero2 )
+# print("Multiplicación:", numero1 * numero2 )
+# print("División:", numero1 / numero2 )
+# print("Moódulo:", numero1 % numero2 )
+# print("División Entera:", numero1 // numero2 )
+# print("Potencia:", numero1 ** numero2 )
+
+
+#Operadores de comparación-------------------------------------------------
+
+x = "Juanita"
+y = 'n'
+z = 2
+a = 5.8
+b = True
+
+print(type(x) == str)
+print(type(y) == str)
+print(type(z) == str)
+print(type(a) == float)
+print(type(b) == bool)
+
+print(1 == 1)
+print(1 != 1)
+print(1 > 2)
+print(2 < 2 )
+print(1 >= 1)
+print(2 <= 3)
+
+#Operadores lógicos----------------------------------------
+
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+print(not False)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
