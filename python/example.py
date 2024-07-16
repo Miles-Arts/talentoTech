937373a7497acaf7658bd8760adc1d9cc54944a7
Milton Figueredo Miles <gratno@gmail.com>
2024-07-16 13:45:12 -0500

    If elif else

--- python/example.py
+++ python/example.py
@@ -112,3 +112,51 @@ import random
 
 
 #Condicionales-------------------------------------------------------
+#IF
+# edad = 20
+
+# if edad > 18:
+#     print(f"Mayor a 18")
+
+#IF ELSE------------------------------------------------------------
+#IF es condicion
+
+# edad = input("Ingrese su edad: " )
+
+# edad = int(edad)
+
+# if edad > 18:
+#     print("Adulto")    
+
+# else:
+#     print("Menor de edad")
+
+
+#IF elif ELSE----------------------------------------------------
+
+edad = input("Ingrese su edad: " )
+edad = int(edad)
+
+if edad < 18:
+    print(f"Es menor de edad tiene: {edad} años.")
+
+elif edad >= 18 and edad < 60:
+    print(f"Es adulto tiene: {edad} años.")
+
+else:
+    print(f"Es adulto mayor tiene: {edad} años.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
