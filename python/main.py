5368b6541f5d2111958175d48c686a0014d72191
Milton Figueredo Miles <gratno@gmail.com>
2024-07-17 07:39:04 -0500

    Condicionales

--- python/main.py
+++ python/main.py
@@ -1,3 +1,4 @@
+import random
 #Example
 #
 #
@@ -94,14 +95,81 @@
 
 
 
-print(1 == 1)
-print(1 == 2)
-print(1 != 2)
-print(1 > 2)
-print(2 < 2 )
-print(1 >= 2)
-print(1 <= 2)
+# print(1 == 1)
+# print(1 == 2)
+# print(1 != 2)
+# print(1 > 2)
+# print(2 < 2 )
+# print(1 >= 2)
+# print(1 <= 2)
 
+
+#Operadores lógicos---------------------------------------------
+
+# print(True and True)
+# print(True and False)
+# print(False and True)
+# print(False and False)
+# print(True or True)
+# print(True or False)
+# print(False or True)
+# print(False or False)
+# print(not False)
+
+
+
+# numero1 = 1
+# numero2 = 2
+# numero3 = 3
+
+# res1 = ( numero1 < numero2) and ( numero1 < numero3 )
+# res2 = ( numero1 < numero2) and ( numero2 > numero3 )
+# res3 = ( numero1 < numero2) or ( numero2 > numero3 )
+# res4 = ( numero1 < numero2) or ( numero2 > numero3 )
+# res5 = ( numero1 != numero2) or ( numero2 <= numero3 )
+# res6 = not( numero1 != numero2) and ( numero2 <= numero3 )
+# res7 = not( numero1 >= numero2) or ( numero2 <= numero3 )
+
+# print(res1)
+# print(res2)
+# print(res3)
+# print(res4)
+# print(res5)
+# print(res6)
+# print(res7)
+
+
+# numero1 = 2
+# numero2 = 5
+
+# print("Resultado:", numero1 * numero2 // 3)
+# print("Resultado:", (numero1 * numero2) // 3)
+# print("Resultado:", numero1 * (numero2 // 3))
+
+
+# import math
+# help(math)
+# math.sqrt(9)
+# math.log()
+
+
+# random.random()
+# numero1 =((random.random()) * 10) % ( 6 + 1 )
+# numero2 =((random.random()) * 10) % ( 6 + 1 )
+# numero3 =((random.random()) * 10) % ( 6 + 1 )
+# print("Resultado",round(numero1)) 
+# print("Resultado",round(numero2)) 
+# print("Resultado",round(numero3)) 
+
+# numero =((random.random())*10)
+
+
+#Condicionales----------------------------------------------------
+# IF
+edad = 20
+
+if edad > 18:
+    print(f"Mayor a 18")
 
 
 
