9599de53de0262acede6f72ed8820023083da303
Milton Figueredo Miles <gratno@gmail.com>
2024-07-17 07:02:40 -0500

    examples class

--- python/main.py
+++ python/main.py
@@ -1,12 +1,106 @@
 #Example
 #
 #
+#Operadores asignación-------------------------
+
+# numero = 1
+# suma = 2 + 4
+# c = suma + numero * 3
+# print("Valor de C es:", c)
+
+# x,y,z = 1, 2, 3
+# print("x =", x, "| y =", y,"| z =", z)
+
+# numero = 5
+# numero = numero + 1
+
+# print("Valor número:", numero)
+
+# numero += 1
+# print("Valor numero:", numero)
+
+
+# numero1 = int(input("Ingrese el primer número: "))
+# numero2 = int(input("Ingrese el segundo número: "))
+
+# suma = numero1 + numero2
+# resta = numero1 - numero2
+# multiplicacion = numero1 * numero2
+# division = numero1 / numero2
+# divisionEntera = numero1 // numero2
+# modulo = numero1 % numero2
+# potencia = numero1 ** numero2
+
+# print(suma)
+# print(resta)
+# print(multiplicacion)
+# print(division)
+# print(divisionEntera)
+# print(modulo)
+# print(potencia)
+
+# Ser claro en la declaración de variables fuciones ETC
+# numeroProducto = 1
+# numero = 1
+# numero = 5
+# numero = numero + 1
+
+# print(numero)
+
+# # 
+# numero = numero + 1   
+# numero += 1 #Se le suma 1 numero
+
+
+# resultado = (1 + 2  + numero) * 3
+# resultado = 1 + 2  + (numero * 3)
+# print(resultado)
+
+# Ordenamiento
+# Recursividad 
+
+# x = "Juanita"
+# y = 'n'
+# z = 2
+# a = 5.8
+# b = True
+
+# print(type(x) == str)
+# print(type(y) == str)
+# print(type(z) == str)
+# print(type(a) == float)
+# print(type(b) == bool)
+
+# print(1 == 1)
+# print(1 != 1)
+# print(1 > 2)
+# print(2 < 2 )
+# print(1 >= 1)
+# print(2 <= 3)
 
 
 
+# x = "Juanita"
+# y = 'n'
+# z = 2
+# a = 5.8
+# b = True
+
+# print(type(x)  == str   )
+# print(type(y)  == int   )
+# print(type(z)  == float   )
+# print(type(a)  == bool   )
+# print(type(b)  == str   )
 
 
 
+print(1 == 1)
+print(1 == 2)
+print(1 != 2)
+print(1 > 2)
+print(2 < 2 )
+print(1 >= 2)
+print(1 <= 2)
 
 
 
