1df94b904568c96bf0c36ba2b11e0ba70a142f95
Milton Figueredo Miles <gratno@gmail.com>
2024-07-19 06:55:35 -0500

    Condicioanles Anidados

--- python/condicionales.py
+++ python/condicionales.py
@@ -43,27 +43,50 @@
 # else:
 #     print(f"Es adulto mayor tiene: {edad} años.")
 
-#IF
-edad = 20
+#IF----------------------------------
+#ELIF----------------------------------
+#ELse----------------------------------
+# edad = 90
 
-if edad < 18:
-    print(f"Menor a 18")
-    print(f"tiene {edad} años.")
+# if edad < 18:
+#     print(f"Menor a 18")
+#     print(f"tiene {edad} años.")
 
-elif edad > 18 and edad <= 60:
-    print(f"Es Adulto")
-    print(f"tiene {edad} años.")
+# elif edad > 18 and edad <= 25:
+#     print(f"Es Joven")
+#     print(f"tiene {edad} años.")
 
-else:
-    print(f"Mayor de edad")
-    print(f"tiene {edad} años.")
+# elif edad >= 25 and edad <= 60:
+#     print(f"Es Adulto")
+#     print(f"tiene {edad} años.")
 
-print("fin")    
+# else:
+#     print(f"Mayor de edad")
+#     print(f"tiene {edad} años.")
 
+# print("fin")    
 
+#Condicional anidado-----------------------------------
+# edad = 9
 
+# if edad < 18:
+#     print(f"Menor a 18")
+#     print(f"tiene {edad} años.")
 
+#     #Condicional anidado
+#     # 1 < edad <= 12:
+#     if edad > 1 and edad <= 12:
+#         print(f"Menor de edad")
 
+#     elif edad > 12:
+#         print("Adolesente")
+
+#     else:
+#         print("Es un bebé")
+
+# print("fin")   
+
+# --------
 
 
 
